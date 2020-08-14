2518b14d1c6fd58d3b160996e4027e8d6e1f1748
melih <melihteke@gmail.com>
2020-08-14 14:40:57 +0200

    added 3rd vlan deletion script. Added some comments and also net_connect.find_prompt()

---
+++ study_nornir2.py
@@ -0,0 +1,7 @@
+from nornir import InitNornir
+from nornir.plugins.tasks.networking import netmiko_send_command
+from nornir.plugins.functions.text import print_result
+
+nr = InitNornir()
+result = nr.run(task=netmiko_send_command, command_string="show tech-support")
+print_result(result)
